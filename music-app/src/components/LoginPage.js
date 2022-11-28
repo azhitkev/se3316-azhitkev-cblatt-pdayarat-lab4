4e2069b4d798cd0f6ab6baee6c49e15e4ecb0639
azhitkev <90805408+azhitkev@users.noreply.github.com>
2022-11-28 01:00:56 -0500

    firebase stuff set up
    - registration
    - login
    - logout
    - email verification (only thing is login is still allowed without verification)

--- music-app/src/components/LoginPage.js
+++ music-app/src/components/LoginPage.js
@@ -1,71 +1,93 @@
-// import "../LoginPage.css";
-
-// export const LoginPage = () => {
-//   return (
-//     <form className="login-wrapper">
-//       <h1>Please Log In</h1>
-//       <label>
-//         <p>Username</p>
-//         <input type="text" />
-//       </label>
-//       <label>
-//         <p>Password</p>
-//         <input type="password" />
-//       </label>
-//       <div>
-//         <button type="submit">Submit</button>
-//       </div>
-//     </form>
-//   );
-// };
-
-import React from "react";
-import axios from "axios";
-import { setAuthToken } from "../helpers/setAuthToken";
+import React, { useEffect, useState } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../firebase-config";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export function LoginPage() {
-  const handleSubmit = (email, password) => {
-    //reqres registered sample user
-    const loginPayload = {
-      email: "eve.holt@reqres.in",
-      password: "cityslicka",
-    };
+  const [user, setUser] = useState({});
+  const [emailLogin, setEmailLogin] = useState(null);
+  const [passwordLogin, setPasswordLogin] = useState(null);
 
-    axios
-      .post("https://reqres.in/api/login", loginPayload)
-      .then((response) => {
-        //get token from response
-        const token = response.data.token;
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
 
-        //set JWT token to local
-        localStorage.setItem("token", token);
+  const handleInputChange = (e) => {
+    //you get the id and value entered in the input box
+    const { id, value } = e.target;
+    //if id is firstName, you set the setFirstName to the value in the input box (so on for the other ones)
+    if (id === "email-login") {
+      setEmailLogin(value);
+    }
+    if (id === "password-login") {
+      setPasswordLogin(value);
+    }
+  };
 
-        //set token to axios common header
-        setAuthToken(token);
+  const login = async () => {
+    if (auth.currentUser.emailVerified) {
+      try {
+        const user = await signInWithEmailAndPassword(
+          auth,
+          emailLogin,
+          passwordLogin
+        );
+        console.log(user);
+      } catch (error) {
+        console.log(error.message);
+      }
+    } else {
+      console.log("user is not verified");
+    }
+  };
 
-        //redirect user to home page
-        window.location.href = "/dashboard";
-      })
-      .catch((err) => console.log(err));
+  const logout = async () => {
+    await signOut(auth);
+    console.log("logged out");
   };
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        const [email, password] = event.target.children;
-        handleSubmit(email, password);
-      }}
-    >
-      <label for="email">Email</label>
-      <br />
-      <input type="email" id="email" name="email" />
-      <br />
-      <label for="password">Password</label>
-      <br />
-      <input type="password" id="password" name="password" />
-      <br></br>
-      <input type="submit" value="Submit" />
-    </form>
+    <div className="form">
+      <h3>Login</h3>
+      <div className="form-body">
+        <div className="email">
+          <label className="form__label" for="email">
+            Email{" "}
+          </label>
+          <input
+            type="email"
+            id="email-login"
+            value={emailLogin}
+            onChange={(e) => handleInputChange(e)}
+            className="form__input"
+            placeholder="Email"
+          />
+        </div>
+        <div className="password">
+          <label className="form__label" for="password">
+            Password{" "}
+          </label>
+          <input
+            className="form__input"
+            type="password"
+            id="password-login"
+            value={passwordLogin}
+            onChange={(e) => handleInputChange(e)}
+            placeholder="Password"
+          />
+        </div>
+      </div>
+      <div class="footer">
+        <h1>{user?.email}</h1>
+        <button type="submit" onClick={login} class="btn">
+          Login
+        </button>
+        <button type="submit" onClick={logout} class="btn">
+          Log Out
+        </button>
+      </div>
+    </div>
   );
 }
