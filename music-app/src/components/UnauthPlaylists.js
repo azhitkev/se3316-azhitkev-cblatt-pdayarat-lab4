95e2ae20f0a59d90de89ed49282884c7761f9df3
cblatt <cblatt@uwo.ca>
2022-11-29 16:29:15 -0500

    unauht playlists (not finished)

--- music-app/src/components/UnauthPlaylists.js
+++ music-app/src/components/UnauthPlaylists.js
@@ -4,6 +4,12 @@ import { Link } from 'react-router-dom';
 
 export const UnauthPlaylists = () => {
 
+    function clearInfoList(){
+        while(document.getElementById('infoList').firstChild){
+            document.getElementById('infoList').removeChild(document.getElementById('infoList').firstChild);
+        }
+    }
+
     function publicPlaylists(){
         fetch('/api/playlists/list-names')
         .then(res => res.json()
@@ -15,18 +21,70 @@ export const UnauthPlaylists = () => {
                 playlists.removeChild(playlists.firstChild);
             }
 
-            for(let i=0; i<data.length; i++){
+            for(let i=0; i<10; i++){
                 playlists.appendChild(document.createTextNode(data[i].playlist_name + ' by ' + data[i].owner));
+
+                var infoBtn = document.createElement('button');
+                infoBtn.style.height = '20px';
+                infoBtn.style.width = '80px';
+                infoBtn.innerHTML = 'Get Info';
+                infoBtn.addEventListener('click', () => {playlistInfo(data[i].playlist_name, data[i].owner)});
+                playlists.appendChild(infoBtn);
+
+                playlists.appendChild(document.createElement('br'));
+
+                let numPlaylists = playlists.childNodes.length;
+                if(numPlaylists == 10){
+                    break;
+                }
+
+                
+
             }
 
-            // number of playlists
-            /*
-            let numPlaylists = playlists.childNodes.length;
-            playlists.appendChild(document.createTextNode(numPlaylists));
-            */
+            
 
         }))
     };
+    
+    function playlistInfo(playlistName, playlistOwner){
+        fetch('/api/playlists/info/' + playlistName)
+        .then(res => res.json()
+        .then(data => {
+            
+            var infoList = document.getElementById('infoList');
+
+            while(infoList.firstChild){
+                infoList.removeChild(infoList.firstChild);
+            }
+
+            for(let i=0; i<data.length; i++){
+                infoList.appendChild(document.createTextNode(playlistName + ' by ' + playlistOwner));
+                infoList.appendChild(document.createElement('br'));
+
+                infoList.appendChild(document.createTextNode('Number of Tracks: ' + data[i].tracks));
+                infoList.appendChild(document.createElement('br'));
+
+                infoList.appendChild(document.createTextNode('Total Duration: ' + data[i].duration + ' Minutes'));
+                infoList.appendChild(document.createElement('br'));
+
+                var closeBtn = document.createElement('button');
+                closeBtn.style.height = '20px';
+                closeBtn.style.width = '55px';
+                closeBtn.innerHTML = 'Close';
+                closeBtn.addEventListener('click', clearInfoList);
+                infoList.appendChild(closeBtn);
+            }
+
+
+
+
+
+            
+
+            
+        }))
+    }
 
     
 
