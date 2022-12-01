9323aa56ffb6d02e4d00167c1d9cec72b99f81dd
azhitkev <90805408+azhitkev@users.noreply.github.com>
2022-11-30 21:21:07 -0500

    fixed html on unauth search

--- music-app/src/components/UnauthSearch.js
+++ music-app/src/components/UnauthSearch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const UnauthSearch = () => {
   function clearList() {
@@ -68,129 +68,128 @@ export const UnauthSearch = () => {
         );
         infoList.appendChild(document.createElement("br"));
 
-            var youtubeBtn = document.createElement('button');
-            youtubeBtn.style.height = '20px';
-            youtubeBtn.style.width = '55px';
-            youtubeBtn.innerHTML = 'Play';
-            youtubeBtn.addEventListener('click', () => {window.open(data.track_url, '_blank')});
-            
-            infoList.appendChild(youtubeBtn);
-            infoList.appendChild(document.createElement('br'));
+        var youtubeBtn = document.createElement("button");
+        youtubeBtn.style.height = "20px";
+        youtubeBtn.style.width = "55px";
+        youtubeBtn.innerHTML = "Play";
+        youtubeBtn.addEventListener("click", () => {
+          window.open(data.track_url, "_blank");
+        });
 
-            infoList.appendChild(document.createTextNode('Album: ' + data.album_title));
-            infoList.appendChild(document.createElement('br'));
+        infoList.appendChild(youtubeBtn);
+        infoList.appendChild(document.createElement("br"));
 
+        infoList.appendChild(
+          document.createTextNode("Album: " + data.album_title)
+        );
+        infoList.appendChild(document.createElement("br"));
 
-            var genresArr = data.track_genres;
-            genresArr = genresArr.replace(/'/g, '"');
-            genresArr = JSON.parse(genresArr);
+        var genresArr = data.track_genres;
+        genresArr = genresArr.replace(/'/g, '"');
+        genresArr = JSON.parse(genresArr);
 
-            var genreNamesArr = [];
-            
-            for(let i=0; i<genresArr.length; i++){
-                genreNamesArr.push(genresArr[i].genre_title);
-            }
+        var genreNamesArr = [];
 
-            var genreNamesStr = genreNamesArr.join(', ');
+        for (let i = 0; i < genresArr.length; i++) {
+          genreNamesArr.push(genresArr[i].genre_title);
+        }
 
+        var genreNamesStr = genreNamesArr.join(", ");
 
-            
-            infoList.appendChild(document.createTextNode('Genre(s): ' + genreNamesStr));
-            infoList.appendChild(document.createElement('br'));
-            
+        infoList.appendChild(
+          document.createTextNode("Genre(s): " + genreNamesStr)
+        );
+        infoList.appendChild(document.createElement("br"));
 
-            infoList.appendChild(document.createTextNode('Play-Length: ' + data.track_duration));
-            infoList.appendChild(document.createElement('br'));
+        infoList.appendChild(
+          document.createTextNode("Play-Length: " + data.track_duration)
+        );
+        infoList.appendChild(document.createElement("br"));
 
-            infoList.appendChild(document.createTextNode('Date Created: ' + data.track_date_created));
-            infoList.appendChild(document.createElement('br'));
+        infoList.appendChild(
+          document.createTextNode("Date Created: " + data.track_date_created)
+        );
+        infoList.appendChild(document.createElement("br"));
 
-            var closeBtn = document.createElement('button');
-            closeBtn.style.height = '20px';
-            closeBtn.style.width = '55px';
-            closeBtn.innerHTML = 'Close';
-            closeBtn.addEventListener('click', clearInfoList);
-            
-            infoList.appendChild(closeBtn);
+        var closeBtn = document.createElement("button");
+        closeBtn.style.height = "20px";
+        closeBtn.style.width = "55px";
+        closeBtn.innerHTML = "Close";
+        closeBtn.addEventListener("click", clearInfoList);
 
-            
-            
-        }))
+        infoList.appendChild(closeBtn);
+      })
+    );
+  }
 
-    }
-
-    
-    
-
-    
-
-    return(
-        <div id="unauthSearch">
-            <div>
-                <div>
-                    <br />
-                    <Link to="/login" style={{marginLeft: '20px'}}>Login</Link>
-                    <Link to="/register" style={{marginLeft: '20px'}}>Register</Link>
-                    <Link to="/unauth-playlists" style={{marginLeft: '20px'}}>Playlists</Link>
-                    
-                </div>
-                <center>
-                    <span style={{fontSize: '60px', fontFamily: 'Impact'}} >MUSIC TOWN</span>
-                    <br />
-                    <span style={{fontSize: '25px', fontFamily: 'Copperplate'}}>A Web Application for Accessing Music Data</span>
-                </center>
-            </div>
-
-            <div id="searchBar">
-                <br />
-                <center>
-                    <input 
-                        type="text" 
-                        id="textBox" 
-                        size="80" 
-                        placeholder="Enter track, artist, album, or genre">
-                    </input>
-                </center>
-                <br />
-                <center>
-                    <input 
-                        type="button" 
-                        id="searchButton" 
-                        value="search"
-                        onClick={() => {
-                            clearList();
-                            search();
-                        }}>
-                    </input>
-                </center>
-            </div>
-
-            
-            <div id="additionalInfo" style={{textAlign: 'center'}}>
-                <center>
-                <ol id="infoList" style={{paddingLeft: '0', display: 'inline-block'}}>
-                    
-                </ol>
-                </center>
-            </div>
-            
-            
-            
-            <div id="searchResults" style={{textAlign: 'center'}}>
-                
-                    <ol id="searchList" style={{paddingLeft: '0', textAlign: 'left', display: 'inline-block'}}>
-                        
-                    </ol>
-                
-            </div>
-
-            
-            
+  return (
+    <div id="unauthSearch">
+      <div>
+        <div>
+          <br />
+          <Link to="/login" style={{ marginLeft: "20px" }}>
+            Login
+          </Link>
+          <Link to="/register" style={{ marginLeft: "20px" }}>
+            Register
+          </Link>
+          <Link to="/unauth-playlists" style={{ marginLeft: "20px" }}>
+            Playlists
+          </Link>
         </div>
-      </center>
+        <center>
+          <span style={{ fontSize: "60px", fontFamily: "Impact" }}>
+            MUSIC TOWN
+          </span>
+          <br />
+          <span style={{ fontSize: "25px", fontFamily: "Copperplate" }}>
+            A Web Application for Accessing Music Data
+          </span>
+        </center>
+      </div>
 
-      <div id="searchResults">
-        <ol id="searchList"></ol>
+      <div id="searchBar">
+        <br />
+        <center>
+          <input
+            type="text"
+            id="textBox"
+            size="80"
+            placeholder="Enter track, artist, album, or genre"
+          ></input>
+        </center>
+        <br />
+        <center>
+          <input
+            type="button"
+            id="searchButton"
+            value="search"
+            onClick={() => {
+              clearList();
+              search();
+            }}
+          ></input>
+        </center>
+      </div>
+
+      <div id="additionalInfo" style={{ textAlign: "center" }}>
+        <center>
+          <ol
+            id="infoList"
+            style={{ paddingLeft: "0", display: "inline-block" }}
+          ></ol>
+        </center>
+      </div>
+
+      <div id="searchResults" style={{ textAlign: "center" }}>
+        <ol
+          id="searchList"
+          style={{
+            paddingLeft: "0",
+            textAlign: "left",
+            display: "inline-block",
+          }}
+        ></ol>
       </div>
     </div>
   );
