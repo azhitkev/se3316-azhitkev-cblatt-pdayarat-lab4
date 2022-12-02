cbb9ae02acffff6eb842d9bfc423a01267c46517
azhitkev <90805408+azhitkev@users.noreply.github.com>
2022-12-02 13:39:15 -0500

    create routing so that deactivated user cannot log in

--- music-app/src/components/Dashboard.js
+++ music-app/src/components/Dashboard.js
@@ -9,26 +9,59 @@ export const Dashboard = () => {
   const [role, setRole] = useState("");
   const [userName, setUser] = useState("");
   const navigate = useNavigate();
+  // let deactivated = false;
 
   useEffect(() => {
     console.log("WE ARE HERE", auth.currentUser.email);
     if (auth.currentUser !== null) {
-      Axios.get(`http://localhost:4000/roleAndUsername/${auth.currentUser.email}`).then(
-        (response) => {
-          setRole(response.data[0].role);
-          setUser(response.data[0].username);
-        }
-      );
+      Axios.get(
+        `http://localhost:4000/roleAndUsername/${auth.currentUser.email}`
+      ).then((response) => {
+        setRole(response.data[0].role);
+        setUser(response.data[0].username);
+      });
     }
+    // ifUserDeactivated();
   }, []);
+
+  // function ifUserDeactivated() {
+  //   if (role === "deactivated") {
+  //     navigate("/");
+  //     signOut(auth.currentUser);
+  //     console.log("logged out");
+  //     deactivated = true;
+  //   }
+  // }
+
+  const logout = async () => {
+    console.log(auth.currentUser);
+    await signOut(auth);
+    console.log("logged out");
+    navigate("/");
+    alert("Admin has been contacted");
+  };
 
   return (
     <div>
       {" "}
-      
-      <h1 style={{ marginLeft :"20px"}}>Role: {role}</h1>
-      <h3 style={{ marginLeft :"20px"}}>User: {userName}</h3>
-      <UnauthSearch role={role} userName={userName} />
+      {role === "deactivated" && (
+        <h1>
+          Your accounted status is: deactivated. Please contact Admin to fix
+          this.
+        </h1>
+      )}
+      {role === "deactivated" && (
+        <button onClick={logout}>Contact Admin.</button>
+      )}
+      {role !== "deactivated" && (
+        <h1 style={{ marginLeft: "20px" }}>Role: {role}</h1>
+      )}
+      {role !== "deactivated" && (
+        <h3 style={{ marginLeft: "20px" }}>User: {userName}</h3>
+      )}
+      {role !== "deactivated" && (
+        <UnauthSearch role={role} userName={userName} />
+      )}
     </div>
   );
 };
